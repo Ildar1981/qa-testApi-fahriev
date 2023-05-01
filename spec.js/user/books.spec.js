dd1bf3376a110f0a8e6a5d4081a62f5ed1d0f5b0
Ildar1981 <fahriyevi@yandex.ru>
2023-05-01 11:21:49 +0300

    Домашнее задание по тестам апи с книгой-Фахриев

--- spec.js/user/books.spec.js
+++ spec.js/user/books.spec.js
@@ -1,2 +1,53 @@
 import supertest from "supertest";
+import user from '../helper/user'
+import {deleteBook} from '../helper/user'
+import {createBook} from '../helper/user'
+import{getBook} from '../helper/user'
+import{getInfoBook} from '../helper/user' 
 
+describe('user', () => {
+describe('POST /api/v1/login', () => {
+    test('Создание книги', async () => {
+      const input = {
+        "userId": "123",
+        "isbn": "pirates"
+      }
+      const res = await createBook(input)
+      expect(res.status).toEqual(201);
+      
+    })
+  
+  })
+
+  describe('POST /api/v1/login', () => {
+    test('обновление книги', async () => {
+
+      const res = await getInfoBook()
+      expect(res.status).toEqual(200);
+      
+    })
+  
+  })
+
+  describe('user', () => {
+    test('Получение информации о книге', async () => {
+      const res = await getBook()
+      expect(res.status).toEqual(200);
+      
+    })
+  
+  })
+
+  describe('user', () => {
+    test('Удаление книги', async () => {
+        const input = {
+            "isbn": "pirates",
+            "userId": "123"
+          }
+      const res = await deleteBook(input)
+      expect(res.status).toEqual(204);
+      
+    })
+  
+  })
+})
